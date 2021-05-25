12d28a9431c860d04f2cb5ccf3c5dd47a8c139e4
Your Name <you@example.com>
2021-05-25 21:04:00 +0530

    changes

--- deals_car_price_mapping- car_parts updation/deals_car_price_mapping.py
+++ deals_car_price_mapping- car_parts updation/deals_car_price_mapping.py
@@ -34,87 +34,82 @@ for x in car_ids_json:
     if int(x.get('id')) not in car_ids_list:
             car_ids_list.append(int(x.get('id')))   #appending ids of car in a list
 
-print(car_ids_list)
-print(len(car_ids_list))
-
 header = ['service_id', 'mapping_id','price', 'deal_id']
 
 data = []    #array in which all dict will be appended for insert_many in deal_car_price_mapping collection
 
-id_count= 90403543957  # first id to be inserted in deal_car_price_mapping is id_count+1 
+id_count= 90403536277  # first id to be inserted in deal_car_price_mapping is id_count+1 
 
 
-csvfile = open(
-    r'/home/ravi/Documents/service_tbl.csv', 'r')
-reader = csv.DictReader(csvfile)
-def update_global_dict_array(car_id):     #updates data array with new dicts
 
-    
-    for each in reader:
-        row = {}
-        for field in header:
-        # data[field]=each[field]
-        # data[field]=each[field]
-            row[field] = int(each[field])
-        # print(row)
-
+# def update_global_dict_array(car_id):     #updates data array with new dicts
+#     csvfile = open(
+#     r'/home/ravi/Documents/service_tbl.csv', 'r')
+#     reader = csv.DictReader(csvfile)
+#     for each in reader:
+#         row = {}
+#         for field in header:
+#         # data[field]=each[field]
+#         # data[field]=each[field]
+#             row[field] = int(each[field])
+#         # print(row) 
+#         global id_count
+#         # filter_kwargs = {'id': id_count }
+#         data_to_insert ={
+#                 "id":id_count,
+#                 "deal_id":13,
+#                 "work_done":4,
+#                 "car_id":car_id,
+#                 'price':row['price'],
+#                 "service_id":row['service_id'],
+#                 "mapping_id":row['mapping_id'],
+#                 "package_id":None,
+#                 "quantity":1,
+#                 "material_cost":row['price'],
+#                 "labour_cost":0,
+#                 "brand":None,
+#                 "work_done":None,
+#                 "created_at":datetime.now(),
+#                 "updated_at":datetime.now(),
+#                 "brand_1_name":"brand_1_name",
+#                 "brand_1_price":None,
+#                 "brand_2_name":"brand_2_name",
+#                 "brand_2_price":None,
+#                 "brand_3_name":None,
+#                 "brand_3_price":None,
+#                 "brand_4_name":None,
+#                 "brand_4_price":None,
+#                 "strike_through_price":None,
+#                 "material_cost_oes":0,
+#                 "inclusive_of_tax":1,
+#                 "recommended_brand":None
+#                 }
+#         data.append(data_to_insert)        
+#         id_count+=1
+#         print(data_to_insert)
+#         print("........................................................")
         
-        global id_count
-        # filter_kwargs = {'id': id_count }
-        data_to_insert ={
-                # "id":id_count,
-                # "deal_id":13,
-                # "work_done":4,
-                "car_id":car_id,
-                # 'price':row['price'],
-                "service_id":row['service_id'],
-                "mapping_id":row['mapping_id'],
-                # "package_id":None,
-                # "quantity":1,
-                # "material_cost":row['price'],
-                # "labour_cost":0,
-                # "brand":None,
-                # "work_done":None,
-                # "created_at":datetime.now(),
-                # "updated_at":datetime.now(),
-                # "brand_1_name":"brand_1_name",
-                # "brand_1_price":None,
-                # "brand_2_name":"brand_2_name",
-                # "brand_2_price":None,
-                # "brand_3_name":None,
-                # "brand_3_price":None,
-                # "brand_4_name":None,
-                # "brand_4_price":None,
-                # "strike_through_price":None,
-                # "material_cost_oes":0,
-                # "inclusive_of_tax":1,
-                # "recommended_brand":None
-                }
-        data.append(data_to_insert)        
-        id_count+=1
-        break
-        # print(data_to_insert)
-        # print("........................................................")
-        # 90403546626
         
 
-car_ids_list.sort()
-for x in car_ids_list:
-    update_global_dict_array(car_id=x)
+# car_ids_list.sort()
+# print(car_ids_list)
+# for x in car_ids_list:
+#     update_global_dict_array(car_id=x)
      
 
-print(data)
+# print(data)
 # collection1.insert_many(data)    
 
 # update_many to db here , in deal_car_price_mapping
 # print(data)
-# collection1.update_many(
-#   {'id' : {'$gte': 90403536277}},
-#   { '$set': {"work_done": 4} },
-#   upsert= False
-# )
+collection1.update_many(
+  {'id' : {'$gte': 90403536277}},
+  { '$set': {"work_done": 4} },
+  upsert= False
+)
 # print("done Insertion")
 
-
+# delete_kwargs = {'id':{"$gte":90403536277}}
+# collection1.delete_many(delete_kwargs)
 
 
